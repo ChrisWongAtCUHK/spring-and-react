858a0153f29bcdd80bb4772128e7d51c6525f305
Chris Wong <wc38.wong@gmail.com>
2020-03-18 13:58:01 +0800

    Use ESLint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import fetch from 'node-fetch';
+import React, {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import fetch from "node-fetch";
 
 function App () {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        fetch('/api/world', { method: 'POST'})
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    },[])
-    return (
-        <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{message}</h1>
-            </header>
-            <p className="App-intro">
+  useEffect(() => {
+    fetch("/api/world", { method: "POST"})
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  },[]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">{message}</h1>
+      </header>
+      <p className="App-intro">
                 To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    )
+      </p>
+    </div>
+  );
 }
 
 export default App;
