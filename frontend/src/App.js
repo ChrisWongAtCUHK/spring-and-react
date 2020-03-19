25842024c01063e60a94e85acea1d60a11927285
Chris Wong <wc38.wong@gmail.com>
2020-03-19 17:13:10 +0800

    Get customer list and add customer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,96 @@
-import React, {useState, useEffect} from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import fetch from "node-fetch";
 
-function App () {
-  const [message, setMessage] = useState("");
+function App() {
+  const [customers, setCustomers] = useState([]);
+  const [customerName, setCustomerName] = useState("");
+  const [email, setEmail] = useState("");
 
+  // page onLoad
   useEffect(() => {
-    fetch("/api/world", { method: "POST"})
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
+    fetch("/api/getAllCustomers")
+      .then(response => response.json())
+      .then(customerList => {
+        setCustomers(Array.from(customerList));
       });
-  },[]);
+  }, []);
+
+  // form submit
+  const submitCustomer = (e) => {
+    e.preventDefault();
+
+    const customer = {
+      customerName,
+      email
+    };
+    fetch("/api/addCustomer", {
+      method: "post",
+      body: JSON.stringify(customer),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(response => response.json())
+      .then(customer => {
+        // update customer list
+        setCustomers([...customers, customer]);
+
+        // clean up the form
+        setCustomerName("");
+        setEmail("");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">{message}</h1>
-      </header>
-      <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <h1>Customer Mart</h1>
+      <form onSubmit={submitCustomer}>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="2">Add/Edit customer</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Customer Name</td>
+              <td><input type="text"
+                onChange={event => setCustomerName(event.target.value)}
+                value={customerName} /></td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td><input type="text"
+                onChange={event => setEmail(event.target.value)}
+                value={email} /></td>
+            </tr>
+            <tr>
+              <td colSpan="2"><input type="submit" value="Submit"
+                className="blue-button" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            <th>Email</th>
+            <th>Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            customers.map(customer => {
+              return (
+                <tr key={customer.id}>
+                  <td>{customer.customerName}</td>
+                  <td>{customer.email}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
